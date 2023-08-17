9509b9543dda8b167443192fc70ce99e924c04fc
sanjh chhatrasal <115942174+sanjhchhatrasal@users.noreply.github.com>
2023-08-17 20:28:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const year = document.getElementById("year");
 
 const today = new Date();
 
-const weekDays = ["Monday", "Tuesday"," Wednesday", "Thursday", "Friday"];
+const weekDays = ["Sunday","Monday", "Tuesday"," Wednesday", "Thursday", "Friday", "Saturday"];
 const allMonth = ["January", "February", "March", "April"," May", "June", "July", "August", "September", "October", "November" , "December"]
 
 date.innerHTML = (today.getDate()<10?"0":"") + today.getDate();
